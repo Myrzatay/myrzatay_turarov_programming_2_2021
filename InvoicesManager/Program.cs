d7d591be581c061ec6b47f4ed2292bb873f00a0f
Myrzatay <86838185+Myrzatay@users.noreply.github.com>
2021-07-19 18:43:23 +0200

    myrzatay_turarov_programming_2_2021
    myrzatay_turarov_programming_2_2021

---
+++ InvoicesManager/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace InvoicesManager
+{}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+            string path = System.IO.Directory.GetCurrentDirectory()+"/"+file;
+            if(System.IO.File.Exists(path)){
+                string[] lines = System.IO.File.ReadAllLines(path);
+                Console.WriteLine(lines[0]);
+                decimal total = 0;
+                string[] dates = new string[lines.Length-1];
+                for(int i=1; i<lines.Length; i++){
+                    string[] line = lines[i].Split(';');
+                    total += decimal.Parse( line[0]);
+                    dates[i-1] = line[2];
+                    Console.WriteLine(lines[i]);
+                }
+                var distinctDates = dates.Distinct();
+                int n=0;
+                foreach(var type in distinctDates){
+                    n++;
+                }
+                Console.WriteLine($"Total expenses:{total}");
+                Console.WriteLine($"Dates of payment: {n}");
+                
+            }
+            else{
+                Console.WriteLine("Incorrect input!");
+            }
+        }
+    }
+
